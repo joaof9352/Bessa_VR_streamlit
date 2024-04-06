e069ec2f5c70c0524bd9ad4887c5f27cf53e27a2
João Ferreira <joaof9352@gmail.com>
2024-04-06 17:27:22 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,16 @@ import streamlit as st
 from backend import calcular_classificacao_final
 import pandas as pd
 
+st.set_page_config(
+    page_title="CP - A",
+    page_icon=":robot_face:",
+    layout="centered",
+    initial_sidebar_state="collapsed",
+)
+
 # Streamlit app layout
-st.title("Soccer League Standings Calculator")
+st.markdown("<h1 style='text-align: center;'>Campeonato Portugal - Série A</h1>", unsafe_allow_html=True)
+st.header("", divider='rainbow')
 
 # Dynamically create text inputs for match scores
 matches = [
@@ -21,19 +29,24 @@ for home_team, away_team in matches:
         away_score = st.text_input(f"{away_team} (goals)", key=f"{away_team}_away")
     match_results.append((home_team, away_team, int(home_score or 0), int(away_score or 0)))
 
-if st.button("Calcular tabela"):
+if st.button("Calcular tabela", use_container_width=True):
     final_standings = calcular_classificacao_final(match_results)
     
     # Convert final_standings to a DataFrame for display
     data = {
         "Posição": [i+7 for i in range(len(final_standings))],
         "Team": [team for team, _ in final_standings],
-        "Pontos": [stats['Pontos'] for _, stats in final_standings],
         "Vitórias": [stats['Vitórias'] for _, stats in final_standings],
         "GM": [stats['GM'] for _, stats in final_standings],
-        "GS": [stats['GS'] for _, stats in final_standings]
+        "GS": [stats['GS'] for _, stats in final_standings],
+        "DG": [stats['GM'] - stats['GS'] for _, stats in final_standings],
+        "Pontos": [stats['Pontos'] for _, stats in final_standings]
     }
     standings_df = pd.DataFrame(data)
     
-    # Display the DataFrame
-    st.table(standings_df)
+    def highlight_last_two_rows(s):
+        return ['background-color: red' if i >= len(s) - 2 else '' for i in range(len(s))]
+    styled_df = standings_df.style.apply(highlight_last_two_rows, subset=['Team', 'Pontos', 'Vitórias', 'GM', 'GS', 'DG', 'Posição'])
+    
+    # Exibir o DataFrame com estilo
+    st.dataframe(styled_df, hide_index=True, use_container_width=True)
